774a7a8a5aad15ad0e89625197c06c6d157445c2
pavlo <pavlo.andriiesh@gmail.com>
2015-04-24 20:39:48 +0300

    init

---
+++ js/backbone-study.js
@@ -0,0 +1,144 @@
+(function() {
+
+    window.Todo = {
+        Models: {},
+        Collections: {},
+        Views: {},
+        Router: {}
+    };
+
+    window.template = function (id) {
+        return _.template($('#' + id).html());
+    };
+
+
+    new Todo.Router;
+    Backbone.history.start();
+
+    Todo.Models.Person = Backbone.Model.extend({
+        defaults: {
+            name: 'Guest User',
+            age: 23,
+            occupation: 'Worker'
+        },
+
+        validate: function (attributes) {
+            if (attributes.age < 0) {
+                return 'Age must be positive.';
+            }
+
+            if (!attributes.name) {
+                return 'Every person must have a name.';
+            }
+        }
+    });
+
+    Todo.Collections.People = Backbone.Collection.extend({
+        model: Todo.Models.Person
+    });
+
+    Todo.Views.People = Backbone.View.extend({
+        tagName: 'ul',
+
+        initialize: function() {
+          this.collection.on("add", this.addOne, this);
+        },
+
+        render: function () {
+            this.collection.each(this.addOne, this);
+            return this;
+        },
+
+        addOne: function(person) {
+            var personView = new Todo.Views.Person( {model: person});
+            this.$el.append(personView.render().el);
+        }
+    });
+
+    Todo.Views.Person = Backbone.View.extend({
+        tagName: 'li',
+        className: 'person',
+        id: 'person-id',
+
+        my_template: template('personTemplate'),
+
+        initialize: function () {
+            this.model.on('change', this.render, this);
+            this.model.on('destroy', this.remove, this);
+        },
+
+        events: {
+            "click .edit": "editPerson",
+            "click .delete": "deletePerson"
+        },
+
+        editPerson: function() {
+            var newName = prompt("Please enter the new name", this.model.get('name'));
+            if (!newName) return;
+            this.model.set('name', newName);
+        },
+
+        deletePerson: function() {
+          this.model.destroy();
+        },
+
+        remove: function() {
+            this.$el.remove();
+        },
+
+        render: function () {
+            this.$el.html(this.my_template(this.model.toJSON()));
+            return this;
+        }
+
+    });
+
+    Todo.Views.AddPerson = Backbone.View.extend({
+        el: '#addPerson',
+
+        events: {
+            'submit': 'submit'
+        },
+
+        submit: function(e) {
+            e.preventDefault();
+            var newPersonName = $(e.currentTarget).find('input[type=text]').val();
+            var person = new Todo.Models.Person({ name: newPersonName});
+            this.collection.add(person);
+        }
+
+    });
+
+    var peopleCollection = new Todo.Collections.People([
+        {
+            name: 'Mohit Jain',
+            age: 26
+        },
+        {
+            name: 'Taroon Tyagi',
+            age: 25,
+            occupation: 'web designer'
+        },
+        {
+            name: 'Rahul Narang',
+            age: 26,
+            occupation: 'Java Developer'
+        }
+    ]);
+
+    var person = new Todo.Models.Person;
+    var personView = new Todo.Views.Person({model: person});
+
+    person.on('invalid', function (model, error) {
+        console.log(error); // printing the error message on console.
+    });
+
+    var addPersonView = new Todo.Views.AddPerson({ collection: peopleCollection });
+    var peopleView = new Todo.Views.People({collection: peopleCollection});
+    $(document.body).append(peopleView.render().el);
+
+})();
+
+
+
+
---
+++ js/todo.js
@@ -0,0 +1,77 @@
+(function () {
+
+    var list = document.getElementById('list-of-tasks'),
+        listOfTasks= ["Get up", "Survive", "Go back to bad"];
+
+    var i = 0;
+
+    var toDo = {
+        addTask: function (task,parent) {
+
+            if (!task) return;
+            if (task === "What needs to be done?") return;
+
+            document.getElementById("text").value = "";
+
+            i++;
+
+            var taskWrapper = document.createElement("div");
+            taskWrapper.id = "taskWrapper" + i;
+
+            var checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+            checkbox.id = "task" + i;
+
+            var label = document.createElement('label');
+            label.className = "tasks btn-block";
+            label.htmlFor = "task" + i;
+            label.appendChild(document.createTextNode(task));
+
+            taskWrapper.appendChild(checkbox);
+            taskWrapper.appendChild(label);
+
+            parent.appendChild(taskWrapper);
+        },
+
+        init: function (tasks, parent) {
+
+            for (var i=0; i<tasks.length; i++) {
+                console.log(tasks[0]);
+
+                toDo.addTask(tasks[i], parent);
+            }
+        }
+    };
+
+    toDo.init(listOfTasks, list);
+
+    document.getElementById("text").addEventListener('keydown', keyHandler, false);
+    document.getElementById("button").addEventListener("click",  function() {
+        toDo.addTask(document.getElementById("text").value, list);
+        document.getElementById("text").value = "What needs to be done?";
+    });
+
+    function keyHandler(event) {
+
+        var KEY_CODE = {
+            SPACE: 32,
+            ENTER: 13
+        };
+
+        switch(event.keyCode) {
+            case KEY_CODE.ENTER:
+                toDo.addTask(event.target.value, list);
+                break;
+        }
+    }
+
+}());
+
+
+
+
+
+
+
+
+
