e994fddeb986054521c5c8a11a29772d410b3b98
pavlo <pavlo.andriiesh@gmail.com>
2015-04-27 18:11:29 +0300

    completed todo list using backbone.js

--- js/todo2.js
+++ js/todo2.js
@@ -1,9 +1,10 @@
-(function() {
+jQuery(function () {
     window.Todo = {
-        Models : {},
-        Views : {},
-        Routers : {},
-        Collections : {}
+        Models: {},
+        Views: {},
+        Routers: {},
+        Collections: {},
+        Localstorage: {}
     };
 
     window.template = function (id) {
@@ -13,24 +14,38 @@
     Todo.Models.Task = Backbone.Model.extend({
         defaults: {
             description: "Some thing to do",
-            time: new Date()
+            active: true
         },
 
-        validate: function(attrs) {
-            if (!attrs.description) {
+        validate: function (attrs) {
+            if (!attrs.description || attrs.description.length) {
                 return "Task should have description";
             }
         }
     });
 
     Todo.Views.Task = Backbone.View.extend({
-        className: "task",
+        className: "task fading",
 
         taskTemplate: template('taskTemplate'),
 
-        initialize: function() {
+        initialize: function () {
             this.model.on('change', this.render, this);
             this.model.on('destroy', this.remove, this);
+            var element = this.$el;
+
+            if (!this.model.get("active")) {
+                element.addClass("changeActiveness");
+            }
+
+            setTimeout(function () {
+               element.toggleClass("unfading");
+            }, 0);
+
+            setTimeout(function () {
+                element.toggleClass("unfading");
+                element.toggleClass("fading");
+            }, 0);
         },
 
         events: {
@@ -39,116 +54,158 @@
             "click .delete": "deleteTask"
         },
 
-        render: function() {
+        render: function () {
             this.$el.html(this.taskTemplate(this.model.toJSON()));
+            Todo.Localstorage.saveTasks();
             return this;
         },
 
-        changeActiveness: function() {
+        changeActiveness: function () {
             if (this.$el.hasClass("changeActiveness")) {
                 this.$el.removeClass("changeActiveness");
+                this.model.set("active", true);
             } else {
                 this.$el.addClass("changeActiveness");
+                this.model.set("active", false);
             }
 
             return this.render();
         },
 
-        editTask: function() {
+        editTask: function () {
             var newDescription = prompt("Please enter new description", this.model.get('description'));
-            if (!newDescription) return;
+            if (!newDescription || !newDescription.trim()) {
+                this.changeActiveness();
+                return;
+            }
             this.model.set('description', newDescription);
             this.changeActiveness();
         },
 
-        deleteTask: function() {
+        deleteTask: function () {
+            var element = this.$el;
+
+            element.toggleClass("deleteTaskAnimationBegin");
+
+            setTimeout(function () {
+                element.toggleClass("deleteTaskAnimationEnd");
+            }, 0);
+
+
             this.model.destroy();
+            Todo.Localstorage.saveTasks();
         },
 
-        remove: function() {
-            this.$el.remove();
+        remove: function () {
+            this.changeActiveness();
+            var el = this.$el;
+
+            setTimeout(function () {
+                el.remove();
+            }, 1000);
         }
 
     });
 
-    Todo.Views.ListOfTasks = Backbone.View.extend({
+    Todo.Views.TasksCollection = Backbone.View.extend({
 
         initialize: function () {
             this.collection.on("add", this.addTask, this);
         },
 
-        render: function() {
+        events: {
+            'click': 'click'
+        },
+
+        click: function (e) {
+            console.log(e.target)
+        },
+
+        render: function () {
             this.collection.each(this.addTask, this);
             return this;
         },
 
-        addTask: function(task) {
-            var taskView = new Todo.Views.Task({ model: task });
+        addTask: function (task) {
+            var taskView = new Todo.Views.Task({model: task});
             this.$el.append(taskView.render().el);
         }
 
     });
 
-    Todo.Collections.ListOfTasks = Backbone.Collection.extend({
+    Todo.Collections.TasksCollection = Backbone.Collection.extend({
         model: Todo.Models.Task
     });
 
-    Todo.Views.AddTaskView = Backbone.View.extend({
-        el: '#addTask',
+    Todo.Views.InputView = Backbone.View.extend({
+        el: '#input',
 
         events: {
             'submit': 'submit'
         },
 
-        submit: function(e) {
-            e.preventDefault();
+        submit: function (event) {
+            event.preventDefault();
             var newTaskDescription = document.getElementById("text").value;
-            if (!newTaskDescription) return;
+            if (!newTaskDescription || !newTaskDescription.trim()) {
+                return;
+            }
 
             document.getElementById("text").value = "";
-            var task = new Todo.Models.Task({ description: newTaskDescription});
+            var task = new Todo.Models.Task({description: newTaskDescription});
             this.collection.add(task);
+            Todo.Localstorage.saveTasks();
         }
     });
 
-    var listOfTasksCollection = new Todo.Collections.ListOfTasks([
-        {
-            description: "Get up"
-        },
-        {
-            description: "Survive"
-        },
-        {
-            description: "Go back to bed"
-        }
-    ]);
 
-    var addTaskView = new Todo.Views.AddTaskView({ collection: listOfTasksCollection});
-    var tasksView = new Todo.Views.ListOfTasks({ collection: listOfTasksCollection});
+    // LocalStorage
+
+    Todo.Localstorage.saveTasks = function () {
+        if (!Todo.Localstorage.supportsLocalStorage()) {
+            return false;
+        }
+        window.localStorage.setItem("tasksCollection", JSON.stringify(tasksCollection));
+    };
+
+    Todo.Localstorage.loadTasks = function () {
+        try {
+            console.log(JSON.parse(window.localStorage.getItem("tasksCollection")))
+        } catch (err) {
+            $("h1").html("Ахтунг! Cработала защита!");
+            window.localStorage.setItem("tasksCollection",JSON.stringify([
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true},
+                    {"description": "научиться не клацать где не надо", "active":true}
+
+            ]));
+
+        }
+
+        if (!Todo.Localstorage.supportsLocalStorage()) {
+            return false;
+        }
+        return JSON.parse(window.localStorage.getItem("tasksCollection"));
+    };
+
+    Todo.Localstorage.supportsLocalStorage = function () {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            return false;
+        }
+    };
+
+    var tasksCollection = new Todo.Collections.TasksCollection(Todo.Localstorage.loadTasks());
+    var inputView = new Todo.Views.InputView({collection: tasksCollection});
+    var tasksView = new Todo.Views.TasksCollection({collection: tasksCollection});
     $(".list-of-tasks").append(tasksView.render().el);
 
-}());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
